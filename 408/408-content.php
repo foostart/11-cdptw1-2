badc2c1e882ad8321b5be8ba7a07337218f6a0bf
nguyenhoa99sz <nguyennguyenhoa9c@gmail.com>
2019-09-18 15:04:56 +0700

    test

--- 408/408-content.php
+++ 408/408-content.php
@@ -6,9 +6,10 @@ preg_match_all($pattern_uri, __DIR__, $matches);
 $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <div class="type-567">
     <div class="container">
-        <div class="col-md-12">
+        <div class="col-md-12 ">
             <div class="related_articles clearfix">
                 <div class="related_title">
                     Related Articles
@@ -17,7 +18,7 @@ $url_path = str_replace('\\', '/', $url_path);
                     <ul>
                         <li>
                             <a href="#" class="rh_related_link_image">
-                                <img src="images/images.jpg.jpg" alt=""/>
+                                <img class="img-responsive" src="images/images.jpg.jpg" alt=""/>
                                 <p>Soft and Ultra fine bristles</p>
                             </a>
 
@@ -35,7 +36,7 @@ $url_path = str_replace('\\', '/', $url_path);
                         </li>
                     </ul>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <ul>
                         <li>
                             <a href="#" class="rh_related_link_image">
@@ -61,11 +62,18 @@ $url_path = str_replace('\\', '/', $url_path);
                                         <span class="fn">demouser
                                             <span class="rh-user-rank-mc">Newbie</span>
                                         </span>
-                                        <div>
-                                            <i class="fa fa-star-o">
-                                                0.00
-                                            </i>
-                                        </div>
+                                        <div class="rate">
+                                            <input type="radio" id="star5" name="rate" value="5" />
+                                            <label for="star5" title="text">5 stars</label>
+                                            <input type="radio" id="star4" name="rate" value="4" />
+                                            <label for="star4" title="text">4 stars</label>
+                                            <input type="radio" id="star3" name="rate" value="3" />
+                                            <label for="star3" title="text">3 stars</label>
+                                            <input type="radio" id="star2" name="rate" value="2" />
+                                            <label for="star2" title="text">2 stars</label>
+                                            <input type="radio" id="star1" name="rate" value="1" />
+                                            <label for="star1" title="text">1 star</label>
+                                            </div>
                                         <span>April 11, 2016 at 12:55 pm</span>
                                     </div>
                                 </div>
@@ -82,6 +90,18 @@ $url_path = str_replace('\\', '/', $url_path);
                                         <span class="fn">demouser<span class="rh-user-rank-mc">Newbie
                                             </span>
                                         </span>
+                                            <div class="rate">
+                                            <input type="radio" id="star5" name="rate" value="5" />
+                                            <label for="star5" title="text">5 stars</label>
+                                            <input type="radio" id="star4" name="rate" value="4" />
+                                            <label for="star4" title="text">4 stars</label>
+                                            <input type="radio" id="star3" name="rate" value="3" />
+                                            <label for="star3" title="text">3 stars</label>
+                                            <input type="radio" id="star2" name="rate" value="2" />
+                                            <label for="star2" title="text">2 stars</label>
+                                            <input type="radio" id="star1" name="rate" value="1" />
+                                            <label for="star1" title="text">1 star</label>
+                                            </div>
                                         <div><i class="fa fa-star-o"></i> 0.00</div>
                                         <span class="time">April 11, 2016 at 12:55 pm</span>
                                     </div>
@@ -96,28 +116,22 @@ $url_path = str_replace('\\', '/', $url_path);
                                     <img src="images/images2.jpg" alt=""/>
                                     <div >
                                         <span class="fn">nanetest</span>
-                                        <div >
-                                        </div>
+                                        <div class="rate">
+                                            <input type="radio" id="star5" name="rate" value="5" />
+                                            <label for="star5" title="text">5 stars</label>
+                                            <input type="radio" id="star4" name="rate" value="4" />
+                                            <label for="star4" title="text">4 stars</label>
+                                            <input type="radio" id="star3" name="rate" value="3" />
+                                            <label for="star3" title="text">3 stars</label>
+                                            <input type="radio" id="star2" name="rate" value="2" />
+                                            <label for="star2" title="text">2 stars</label>
+                                            <input type="radio" id="star1" name="rate" value="1" />
+                                            <label for="star1" title="text">1 star</label>
+                                            </div>
                                         <span class="time">May 10, 2016 at 6:58 am</span>
                                     </div>
                                 </div>
                                 <p>This is a test comment</p>
-                            </div>
-                        </li><!-- #comment-## -->
-                        <li class="comment odd alt" id=" 4">
-                            <div class="commbox">
-                                <div class="comment-author clearfix">
-                                    <a rel="nofollow" class="comment-reply-link" href="#">Reply</a>
-                                    <img src="images/images2.jpg" alt=""/>
-                                    <div >
-                                        <span class="fn">bill<span class="rh-user-rank-mc">Newbie
-                                            </span>
-                                        </span>
-                                        <div><i class="fa fa-star-o"></i> 0.00</div>
-                                        <span class="time">May 27, 2016 at 10:55 am</span>
-                                    </div>
-                                </div>
-                                <p>O M G</p>
                             </div>
                         </li><!-- #comment-## -->
                         <li class="comment even" >
@@ -129,33 +143,22 @@ $url_path = str_replace('\\', '/', $url_path);
                                         <span class="fn"><a href="#">dkssud
                                             </a>
                                         </span>
-                                        <div>
-                                            <i class="fa fa-star-o">
-                                                15.00
-                                            </i>
-                                        </div>
+                                        <div class="rate">
+                                            <input type="radio" id="star5" name="rate" value="5" />
+                                            <label for="star5" title="text">5 stars</label>
+                                            <input type="radio" id="star4" name="rate" value="4" />
+                                            <label for="star4" title="text">4 stars</label>
+                                            <input type="radio" id="star3" name="rate" value="3" />
+                                            <label for="star3" title="text">3 stars</label>
+                                            <input type="radio" id="star2" name="rate" value="2" />
+                                            <label for="star2" title="text">2 stars</label>
+                                            <input type="radio" id="star1" name="rate" value="1" />
+                                            <label for="star1" title="text">1 star</label>
+                                            </div>
                                         <span class="time">September 3, 2016 at 5:36 am</span>
                                     </div>
                                 </div>
                                 <p>Test</p>
-                            </div>
-                        </li><!-- #comment-## -->
-                        <li class="comment odd alt" >
-                            <div class="commbox">
-                                <div class="comment-author clearfix">
-                                    <a rel="nofollow" class="comment-reply-link" href="#">Reply</a>
-                                    <img src="images/images2.jpg" alt=""/>
-                                    <div >
-                                        <span class="fn">
-                                            <a href="#S" target="_blank" rel="nofollow">testor
-                                            </a>
-                                        </span>
-                                        <div >
-                                        </div>
-                                        <span class="time">September 11, 2016 at 9:32 pm</span>
-                                    </div>
-                                </div>
-                                <p>Testor</p>
                             </div>
                         </li><!-- #comment-## -->
                     </ol>
@@ -168,36 +171,24 @@ $url_path = str_replace('\\', '/', $url_path);
                     <h3 >Leave a reply </h3>
                     <form class="form-horizontal" role="form" action="#" method="POST">
                         <div class="form-group">
-                            <div class="lbl col-md-1">
                                 <label>Name</label>
-                            </div>
-                            <div class="ip col-md-3">
-                                <input type="text" name="" id ="name" placeholder ="Name...">
-                            </div>
+                                <div>
+                                <input type="text" name="" id ="name" placeholder ="Name..."></div>
                         </div>
                         <div class="form-group">
-                            <div class="lbl col-md-1">
                                 <label>Email</label>
-                            </div>
-                            <div class="ip col-md-3">
-                                <input type="text" name="" id ="email" placeholder ="Email...">
-                            </div>
+                                <div>
+                                <input type="text" name="" id ="email" placeholder ="Email..."></div>
                         </div>
                         <div class="form-group">
-                            <div class="lbl col-md-1">
                                 <label>Website</label>
-                            </div>
-                            <div class="ip col-md-3">
-                                <input type="text" name="" id ="web" placeholder ="Website...">
-                            </div>
+                                <div>
+                                <input type="text" name="" id ="web" placeholder ="Website..."></div>
                         </div>
                         <div class="form-group">
-                            <div class="lbl col-md-1">
                                 <label>Comment</label>
-                            </div>
-                            <div class="ip col-md-3">
-                                <textarea type="text" name="" id ="comment" placeholder ="Comment..."></textarea>
-                            </div>
+                                <div>
+                                <textarea type="text" name="" id ="comment" placeholder ="Comment..."></textarea></div>
                         </div>
                     </form>
                     <button class="btn btn-primary">Submit Comment</button>
